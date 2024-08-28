f5ec9ea7c7ccb91895a0b014213a2ca6654b7c78
Egor Ivanov <e.ivanov@12storeez.com>
2024-08-28 18:02:41 +0300

    upload base

---
+++ decompile.bat
@@ -0,0 +1,8 @@
+@chcp 65001
+
+@rem запустить выгрузку исходников основной конфигурации
+call vrunner decompile --out src/cf -с
+
+@rem запустить выгрузку исходников расширения ИмяРасширения
+@REM call vrunner decompileext Галамарт src/cfe/ИмяРасширения
+
--- designer.cmd
+++
@@ -1,2 +0,0 @@
-@chcp 65001
-call vrunner designer --no-wait %*
