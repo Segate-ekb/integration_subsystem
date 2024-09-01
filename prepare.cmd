8711c6d06c11b282fa5b67894af6081eb1c99ccc
Egor Ivanov <e.ivanov@12storeez.com>
2024-09-01 15:31:12 +0300

    Добавил снятие безопасного режима и защиты от опасных действий для yaxunit

--- prepare.cmd
+++ prepare.cmd
@@ -12,7 +12,10 @@ call vrunner init-dev --src src/cf %*
 @rem call vrunner compileepf src/erf/МойВнешнийОтчет build %*
 
 @rem собрать расширения конфигурации внутри ИБ
-call vrunner compileext src/cfe/YAXUnit YAXUnit %*
+@REM call vrunner compileext src/cfe/YAXUnit YAXUnit %*
+call vrunner compileexttocfe --src src/cfe/YAXUNIT --out build/YAXUNIT.cfe %*
+
+call vrunner run --command "Путь=build/YAXUNIT.cfe;ЗавершитьРаботуСистемы;" --execute $runnerRoot\epf\ЗагрузитьРасширениеВРежимеПредприятия.epf %*
 
 @rem Обновление в режиме Предприятия
 call vrunner run --command "ЗапуститьОбновлениеИнформационнойБазы;ЗавершитьРаботуСистемы;" --execute $runnerRoot\epf\ЗакрытьПредприятие.epf %*
