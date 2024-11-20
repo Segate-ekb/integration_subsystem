027b46318bc4458d6ec2a098a3344036dda0a0f8
Egor Ivanov <e.ivanov@12storeez.com>
2024-11-20 17:08:19 +0300

    Убрал гит инкремент

--- update.cmd
+++ update.cmd
@@ -1,7 +1,7 @@
 @chcp 65001
 
 @rem обновление конфигурации основной разработческой ИБ без поддержки или на поддержке. по умолчанию в каталоге build/ib
-call vrunner update-dev --src src/cf --disable-support --git-increment
+call vrunner update-dev --src src/cf --disable-support
 
 @rem обновление конфигурации основной разработческой ИБ из хранилища. для включения раскомментируйте код ниже
 @rem call vrunner loadrepo %*
